7db5dfe7e1ebed300671f9ab35b257cf0d37d052
lzzking0216-ui <lzz.king0216@gmail.com>
2026-07-08 10:48:17 +0800

    消除build多余文件

--- build/Desktop_Qt_6_8_3_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_DeckLab_FILE E:/DeckLab-2.0/build/Desktop_Qt_6_8_3_MSVC2022_64bit-Debug/DeckLab.exe)
-set(__QT_DEPLOY_TARGET_DeckLab_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_DeckLab_RUNTIME_DLLS E:/QT_software/QT/6.8.3/msvc2022_64/bin/Qt6Widgetsd.dll;E:/QT_software/QT/6.8.3/msvc2022_64/bin/Qt6Networkd.dll;E:/QT_software/QT/6.8.3/msvc2022_64/bin/Qt6Guid.dll;E:/QT_software/QT/6.8.3/msvc2022_64/bin/Qt6Cored.dll)
